2cf398c9c05fec64f5f5371967955ec8a8dd70df
SimoneMicucciSapienza <micucci.1934642@studenti.uniroma1.it>
2023-05-23 16:48:02 +0200

    sm: implemented set table to remove switch and little work on bitmap/Makefile

--- bitmap/bitmap.c
+++ bitmap/bitmap.c
@@ -5,6 +5,31 @@
 #include <signal.h>
 #include <stdio.h>
 
+// like avr table
+static const uint8_t _set_up_table [] =
+{
+	0xfe,
+	0x7f,
+	0xbf,
+	0xdf,
+	0xef,
+	0xf7,
+	0xfb,
+	0xfd
+};
+
+static const uint8_t _set_dw_table [] =
+{
+	0x01,
+	0x80,
+	0x40,
+	0x20,
+	0x10,
+	0x08,
+	0x04,
+	0x02
+};
+
 //	bitmap init
 void	BitMap_init(BitMap* this, int num_bits, uint8_t* buffer){
 	this->buffer = buffer;
@@ -19,57 +44,12 @@ void	BitMap_set(BitMap* this, int pos_bit, uint8_t status){
 		return;
 	if (this->num_bits < pos_bit)
 		return;
-	//setting bit with switch map
-	switch (pos_bit%8){
-		case 0:			//8^ bit, slide back: 0000-0001 & 1111-1110 (01 & fe)
-			if (status)
-				this->buffer[(pos_bit/8) -1] |= 0x01;
-			else
-				this->buffer[(pos_bit/8) -1] &= 0xfe;
-			return;
-		case 1:			//1^ bit: 1000-0000 & 0111-1111 (80 & 7f)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x80;
-			else
-				this->buffer[pos_bit/8] &= 0x7f;
-			return;
-		case 2:			//2^ bit: 0100-0000 & 1011-1111 (40 & bf)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x40;
-			else
-				this->buffer[pos_bit/8] &= 0xbf;
-			return;
-		case 3:			//3^ bit: 0010-0000 & 1101-1111 (20 & df)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x20;
-			else
-				this->buffer[pos_bit/8] &= 0xdf;
-			return;
-		case 4:			//4^ bit: 0001-0000 & 1110-1111 (10 & ef)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x10;
-			else
-				this->buffer[pos_bit/8] &= 0xef;
-			return;
-		case 5:			//5^ bit: 0000-1000 & 1111-0111 (08 & f7)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x08;
-			else
-				this->buffer[pos_bit/8] &= 0xf7;
-			return;
-		case 6:			//6^ bit: 0000-0100 & 1111-1011 (04 & fb)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x04;
-			else
-				this->buffer[pos_bit/8] &= 0xfb;
-			return;
-		case 7:			//7^ bit: 0000-0010 & 1111-1101 (02 & fd)
-			if (status)
-				this->buffer[pos_bit/8] |= 0x02;
-			else
-				this->buffer[pos_bit/8] &= 0xfd;
-			return;
-	}
+	//smarter way to set bit and more readable (if you ignore the tables)
+	if (status)
+		this->buffer[(pos_bit/8)-(!(pos_bit%8))] |= _set_dw_table [pos_bit%8];
+	else
+		this->buffer[(pos_bit/8)-(!(pos_bit%8))] &= _set_up_table [pos_bit%8];
+	return;
 }
 
 //	bitmap get
@@ -77,29 +57,13 @@ uint8_t	BitMap_get(BitMap* this, int pos_bit){
 	if (this->num_bits < pos_bit)
 		return 2;
 	uint8_t target = this->buffer[(pos_bit/8) - (!pos_bit%8)];
-	switch (pos_bit%8) {		//not best implementation but good enouth for now
-		case 0 :		//8^ bit:
-			return (uint8_t)(target & 0x01);
-		case 1 :		//1^ bit:
-			return (uint8_t)(target & 0x80);
-		case 2 :		//2^ bit:
-			return (uint8_t)(target & 0x40);
-		case 3 :		//3^ bit:
-			return (uint8_t)(target & 0x20);
-		case 4 :		//4^ bit:
-			return (uint8_t)(target & 0x10);
-		case 5 :		//5^ bit:
-			return (uint8_t)(target & 0x08);
-		case 6 :		//6^ bit:
-			return (uint8_t)(target & 0x04);
-		case 7 :		//7^ bit:
-			return (uint8_t)(target & 0x02);
-	}
-	return 2;
+	//smart way to get bit and more readable
+	return (uint8_t)(target & _set_dw_table[pos_bit%8]);
 }
 
 //	bitmap print
 void	BitMap_print(BitMap* this){
+	//shit test wt a printf, M A J E S T I C
 	printf("bitmap:\t%s%p%s",(!this)?ORANGE:YELLOW,this,RESET);
 	if (!this){
 		printf("\n");
@@ -136,13 +100,13 @@ uint32_t	BitMap_getBytes(BitMap* this, int pos_bit){
 	//shit test (whitout mercy :-D )
 	if (!this){
 		//sadly I din't find any other way to comunicate a significative message over kill program
-		fprintf(STDERR_FILENO,"%sERROR%s null pointer exception\n",RED,RESET);
+		dprintf(STDERR_FILENO,"%sERROR%s null pointer exception\n",RED,RESET);
 		raise(SIGKILL);
 		pause();
 	}
 	if (this->num_bits<pos_bit){
 		//sadly I din't find any other way to comunicate a significative message over kill program
-		fprintf(STDERR_FILENO,"%sERROR%s request out of boundaries\n",ORANGE,RESET);
+		dprintf(STDERR_FILENO,"%sERROR%s request out of boundaries\n",ORANGE,RESET);
 		raise(SIGKILL);
 		pause();
 	}
