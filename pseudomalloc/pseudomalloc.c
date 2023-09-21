a8dfa72bb19a6b0b1abacd896aaded5bbb7afa66
Simone Micucci <micucci.1934642@studenti.uniroma1.it>
2023-09-21 14:42:18 +0200

    SM: implemented pseudo alloc, missing pointer filter for pseudo free

--- pseudomalloc/pseudomalloc.c
+++ pseudomalloc/pseudomalloc.c
@@ -6,28 +6,49 @@
 
 //	function called by default before main
 void	__attribute__ ((constructor)) pseudoCreate(){
-	mhelper_create(helper.mhelper);		//generate memory helper
-	buddy_init(
+	// generate memory helper
+	mhelper_create(&(helper.mhelper));
+	// initialize buddy with memory helper
+	buddy_init(	&(helper.buddy),
+			mhelper_alloc(&(helper.mhelper),(1024*1024)),
+			mhelper_alloc(&(helper.mhelper),(31*128));
+	// initialize big slab with memory helper
+	slabInit(	&(helper.big_slab),
+			mhelper_alloc(&(helper.mhelper),(64*1024)),
+			mhelper_alloc(&(helper.mhelper),(128)),
+			1024,
+			64);
+	// initialize small slab with memory helper
+	slabInit(	&(helper.small_slab),
+			mhelper_alloc(&(helper.mhelper),(16*1024)),
+			mhelper_alloc(&(helper.mhelper),(128)),
+			1024,
+			16);
 }
 
 //	function called by default after main
 void	__attribute__ ((destructor)) pseudoDestroy(){
+	mhelper_destroy(&(helper.mhelper));
 }
 
 //	switch between all avaible allocator given to allocate a buffer of given size
 void*	pseudoAlloc(size_t size){
 	// selector
-	if (size > 1024) {
+	if (size > 1016) {	//buddy require space to store the size
 		// call mhelper
+		return mhelper_alloc(&(helper.mhelper),size);
 	}
 	else if (size > 64) {
 		// call buddy
+		return buddy_alloc(&(helper.buddy),size);
 	}
 	else if (size > 16) {
-		// call slab 1
+		// call big slab
+		return slabAllocator(&(helper.big_slab));
 	}
 	else {
-		// call slab 2
+		// call small slab
+		return slabAllocator(&(helper.small_slab));
 	}
 }
 
